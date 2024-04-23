d08abbf60f738ee9fc4c1f730185d2b43c68addb
seed-yoo <lukdown10@naver.com>
2024-04-23 11:45:43 +0900

    ....

---
+++ bin/ex04.dart
@@ -0,0 +1,31 @@
+
+void main() {
+  //연산자
+  // +, -, *, /, %  --> 사칙 연산자 동일하다
+
+  double result = 5/2;
+  print(result);
+
+  int result2 = 5%2;
+  print(result2); //나머지
+
+  int result3 = 5 ~/ 2;
+  print(result3); // 몫
+
+  print("---------------------------");
+  int no = 3;
+  no++;
+  print(no);
+
+  print("---------------------------");
+  bool boo = (5>3) && (5<3); // true  &&  false --> false
+  print(boo);
+
+
+
+
+
+
+
+  
+}
